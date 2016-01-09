af222ad01a2850b832c4a4facfc4b21639ac6ade
trebloc <ctse77@alum.berkeley.edu>
2016-01-09 14:55:33 -0800

    setting up the creature create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
+# db/seeds.rb
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Creature.create({name: "Yoda", description: "Little green man"})
+Creature.create({name: "Luke", description: "Jedi"})
+Creature.create({name: "Darth Vader", description: "Father of Luke"})
